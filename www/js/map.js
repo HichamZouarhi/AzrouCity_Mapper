a25fb20de52697cc158dc2a97b00461c8a2e7a70
mapping <hicham.zouarhi@gmail.com>
2016-08-19 09:25:34 +0000

    refined search menu and geolocation

--- www/js/map.js
+++ www/js/map.js
@@ -12,4 +12,48 @@ var map= new ol.Map({
 		projection: 'EPSG:4326',
 		zoom: 14
 	})
-})
+});
+
+$('#geolocateMe').click(function(){
+	var geolocation = new ol.Geolocation({
+		projection: 'EPSG:4326'
+	});
+	geolocation.setTracking(true);
+
+	var accuracyFeature = new ol.Feature();
+	geolocation.on('change:accuracyGeometry', function() {
+		accuracyFeature.setGeometry(geolocation.getAccuracyGeometry());
+	});
+
+	var positionFeature = new ol.Feature();
+	positionFeature.setStyle(new ol.style.Style({
+		image: new ol.style.Circle({
+			radius: 6,
+			fill: new ol.style.Fill({
+				color: '#3399CC'
+			}),
+			stroke: new ol.style.Stroke({
+				color: '#fff',
+				width: 2
+			})
+		})
+	}));
+
+	geolocation.on('change:position', function() {
+		var coordinates = geolocation.getPosition();
+		positionFeature.setGeometry(coordinates ? new ol.geom.Point(coordinates) : null);
+	});
+
+	geolocation.on('error', function(error) {
+		console.log(error);
+	});
+
+	var myPosition=new ol.layer.Vector({
+		map: map,
+		source: new ol.source.Vector({
+			features: [accuracyFeature, positionFeature]
+		})
+	});	
+});
+
+// ends here
