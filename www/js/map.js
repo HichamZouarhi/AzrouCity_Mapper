3ab8f9760e033064e7888ce22da9f3f1f04f40b9
mapping <hicham.zouarhi@gmail.com>
2016-08-23 08:58:15 +0000

    Geolocation + cluster of the features ( hotels and banks )

--- www/js/map.js
+++ www/js/map.js
@@ -57,3 +57,114 @@ $('#geolocateMe').click(function(){
 });
 
 // ends here
+
+// vector layers containing data from the geojson files
+var layers = new Object();
+
+var clusterSourceBank = new ol.source.Cluster({
+	distance: 40,
+	source: sources['bank']
+});
+
+var clusterSourceHotel = new ol.source.Cluster({
+	distance: 40,
+	source: sources['hotel']
+});
+
+var styleCache = {};
+layers['bank'] = new ol.layer.Vector({
+	source: clusterSourceBank,
+	visible: false,
+	style: function(feature) {
+		var size = feature.get('features').length;
+		var style = styleCache[size];
+		if (!style) {
+			style = new ol.style.Style({
+				image: new ol.style.Circle({
+					radius: 10,
+					stroke: new ol.style.Stroke({
+						color: '#fff'
+					}),
+					fill: new ol.style.Fill({
+						color: '#3399CC'
+					})
+				}),
+				text: new ol.style.Text({
+					text: size.toString(),
+					fill: new ol.style.Fill({
+						color: '#fff'
+					})
+				})
+			});
+			styleCache[size] = style;
+		}
+		if(size==1){
+			console.log('length of the cluster is 1');
+			return (new ol.style.Style({
+				image: new ol.style.Icon({
+					anchor: [0.5, 0],
+					anchorOrigin: 'bottom-left',
+					anchorXUnits: 'fraction',
+					anchorYUnits: 'pixels',
+					src: 'img/marker-bank.png'
+				})
+			}));
+		}
+		return style;
+	}
+});
+
+layers['hotel'] = new ol.layer.Vector({
+	source: clusterSourceHotel,
+	visible: false,
+	style: function(feature) {
+		var size = feature.get('features').length;
+		var style = styleCache[size];
+		if (!style) {
+			style = new ol.style.Style({
+				image: new ol.style.Circle({
+					radius: 10,
+					stroke: new ol.style.Stroke({
+						color: '#fff'
+					}),
+					fill: new ol.style.Fill({
+						color: '#3399CC'
+					})
+				}),
+				text: new ol.style.Text({
+					text: size.toString(),
+					fill: new ol.style.Fill({
+						color: '#fff'
+					})
+				})
+			});
+			styleCache[size] = style;
+		}
+		if(size==1){
+			return (new ol.style.Style({
+				image: new ol.style.Icon({
+					anchor: [0.5, 0],
+					anchorOrigin: 'bottom-left',
+					anchorXUnits: 'fraction',
+					anchorYUnits: 'pixels',
+					src: 'img/marker-hotel.png'
+				})
+			}));
+		}
+		return style;
+	}
+});
+
+map.addLayer(layers['bank']);
+map.addLayer(layers['hotel']);
+$(".selectCategory").click(function(){
+	console.log(this.id);
+	
+		for( var layer in layers){
+			layers[layer].setVisible(false);
+		}
+		if(layers[this.id]){
+			layers[this.id].setVisible(true);
+		}
+	
+});
